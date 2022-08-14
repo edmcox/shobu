218845142a634a980a878a1b6d234115ad681596
Ed Cox <ed.cox1996@gmail.com>
2022-08-14 22:56:41 +0100

    added win checker and illegal move tracker

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from board import BoardState
-from check import check_move, check_win
 from flask import Flask, request, render_template
 
 board_state = BoardState()
@@ -18,13 +17,15 @@ def home():
 @app.route("/game", methods=["GET", "POST"])
 def game():
 
+    player_move = None
+
     move_1_start = request.form.get("move_1_start")
     move_1_end = request.form.get("move_1_end")
     move_2_start = request.form.get("move_2_start")
     move_2_end = request.form.get("move_2_end")
 
-    if request.method == "POST":
-        board_state.make_move(
+    if request.method == "POST" and not board_state.game_over:
+        player_move = board_state.make_move(
             move_1=[
                 [int(x.strip()) for x in move_1_start.split(",")],
                 [int(x.strip()) for x in move_1_end.split(",")],
@@ -38,17 +39,30 @@ def game():
 
         board_state.plot_board()
 
-        if not multiplayer:
-            random_move_1, random_move_2 = board_state.random_move()[0]
+        board_state.check_win()
+
+        if not multiplayer and not board_state.game_over and not player_move:
+            random_move_1, random_move_2 = board_state.random_move()
+
             board_state.make_move(
                 random_move_1, random_move_2, colour=board_state.current_turn
             )
+
+            board_state.plot_board()
+            board_state.turn_no -= 1
+
+            board_state.check_win()
 
     colour = colours[board_state.current_turn]
     turn_no = board_state.turn_no
 
     return render_template(
-        "game.html", colour=colour, turn_no=turn_no, url="/static/images/board.png"
+        "game.html",
+        colour=colour,
+        turn_no=turn_no,
+        url="/static/images/board.png",
+        player_move=player_move,
+        game_over=board_state.game_over,
     )
 
 
