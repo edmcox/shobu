d4b3c925447724e9322577022edde58929b03d86
Ed Cox <ed.cox1996@gmail.com>
2022-08-14 18:43:36 +0100

    added app

---
+++ app.py
@@ -0,0 +1,51 @@
+from board import BoardState
+from check import check_move, check_win
+from flask import Flask, request, render_template
+
+board_state = BoardState()
+colours = {1: "Black", 2: "White"}
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    board_state.plot_board()
+    return render_template("home.html")
+
+
+@app.route("/game", methods=["GET", "POST"])
+def game():
+
+    move_1_start = request.form.get("move_1_start")
+    move_1_end = request.form.get("move_1_end")
+    move_2_start = request.form.get("move_2_start")
+    move_2_end = request.form.get("move_2_end")
+
+    if request.method == "POST":
+        for row in board_state.board:
+            print(row)
+        board_state.make_move(
+            move_1=[
+                [int(x.strip()) for x in move_1_start.split(",")],
+                [int(x.strip()) for x in move_1_end.split(",")],
+            ],
+            move_2=[
+                [int(x.strip()) for x in move_2_start.split(",")],
+                [int(x.strip()) for x in move_2_end.split(",")],
+            ],
+            colour=board_state.current_turn,
+        )
+
+        board_state.plot_board()
+
+    colour = colours[board_state.current_turn]
+    turn_no = board_state.turn_no
+
+    return render_template(
+        "game.html", colour=colour, turn_no=turn_no, url="/static/images/board.png"
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
